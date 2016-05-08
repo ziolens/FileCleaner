57b732c921f0018873229d117a512dc2f566f594
ziolens <marek791@gmail.com>
2016-05-08 19:12:43 +0200

    fileCleaner v.1.0

---
+++ FileCleaner/FileCleaner/Cleaner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileCleaner
+{
+    class Cleaner
+    {
+        public void deleteFile(string fileToDelete, List<string> extensions, byte days)
+        {
+            FileInfo fileInfo = new FileInfo(fileToDelete);
+            if (extensions.Contains(fileInfo.Extension) && fileInfo.LastWriteTime < DateTime.Today.AddDays(days * -1))
+            {
+                fileInfo.Delete();
+            }
+        }        
+    }
+}
---
+++ FileCleaner/FileCleaner/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace FileCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cleaner myCleaner = new Cleaner();
+            string path = args[0];
+            byte days = Byte.Parse(args[2]);
+            List<string> extensions = new List<string>(args[1].Split(','));
+            for (int i = 0; i < extensions.Count; i++)
+            {
+                extensions[i] = String.Format(".{0}", extensions[i]);
+            }
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                myCleaner.deleteFile(file, extensions, days);
+            }
+        }        
+    }
+}
